7104219f501ef237309cf727ad7227a6ae62e6df
FannarHrafn <fannarhrafn@gmail.com>
2019-02-08 14:56:39 +0000

    Update code.py

--- code.py
+++ code.py
@@ -15,3 +15,18 @@ def combine(n):
     return list(map("".join,itertools.combinations('abcdefghijklnmopqrstuvwxyz',n)))
 print combine(3)
 print len(combine(3))
+
+def revcombine(n):
+    return list(map("".join,itertools.combinations('abcdefghijklnmopqrstuvwxyz'[::-1],n)))
+
+alist = revcombine(5)
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            print(i)
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+bubbleSort(alist)
+print(alist)
